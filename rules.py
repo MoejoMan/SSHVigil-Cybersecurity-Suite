1a85d240d427dde152348eb6f26fee6747f12b22
jhg56 <jhg567999@gmail.com>
2025-12-28 05:35:51 +0000

    added IPv6 support

--- rules.py
+++ rules.py
@@ -6,28 +6,28 @@ import re
 PATTERNS = [
 	{
 		'name': 'invalid_user',
-		'regex': re.compile(r"Invalid user (?P<username>\S+) from (?P<ip>[\d.]+)"),
+		'regex': re.compile(r"Invalid user (?P<username>\S+) from (?P<ip>[0-9a-fA-F:.]+)"),
 		'success': False,
 	},
 	{
 		'name': 'failed_password_user',
-		'regex': re.compile(r"Failed password for (?P<username>\S+) from (?P<ip>[\d.]+)"),
+		'regex': re.compile(r"Failed password for (?P<username>\S+) from (?P<ip>[0-9a-fA-F:.]+)"),
 		'success': False,
 	},
 	{
 		'name': 'failed_password_invalid',
-		'regex': re.compile(r"Failed password for invalid user (?P<username>\S+) from (?P<ip>[\d.]+)"),
+		'regex': re.compile(r"Failed password for invalid user (?P<username>\S+) from (?P<ip>[0-9a-fA-F:.]+)"),
 		'success': False,
 	},
 	{
 		'name': 'accepted_password',
-		'regex': re.compile(r"Accepted password for (?P<username>\S+) from (?P<ip>[\d.]+)"),
+		'regex': re.compile(r"Accepted password for (?P<username>\S+) from (?P<ip>[0-9a-fA-F:.]+)"),
 		'success': True,
 	},
-    {
-        'name': 'accepted_publickey',
-        'regex': re.compile(r"Accepted publickey for (?P<username>\S+) from (?P<ip>[\d.]+)"),
-        'success': True,
-    },
+	{
+		'name': 'accepted_publickey',
+		'regex': re.compile(r"Accepted publickey for (?P<username>\S+) from (?P<ip>[0-9a-fA-F:.]+)"),
+		'success': True,
+	},
 ]
 
